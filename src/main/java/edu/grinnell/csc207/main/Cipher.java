ce00a4382dbd2fa0b0e8344256c99376d44671bd
gold-leo <goldmanleo@grinnell.edu>
2024-10-02 16:41:06 -0500

    fixed Cipher

--- src/main/java/edu/grinnell/csc207/main/Cipher.java
+++ src/main/java/edu/grinnell/csc207/main/Cipher.java
@@ -4,14 +4,26 @@ import java.io.PrintWriter;
 
 import edu.grinnell.csc207.util.CipherUtils;
 
+/**
+ * Cipher class.
+ */
 public class Cipher {
+  /**
+   * The permitted size of the args array.
+   */
+  private static final int ARGS_SIZE = 4;
+
+  /**
+   * Main function.
+   * @param args
+   */
   public static void main(String[] args) {
     PrintWriter pen = new PrintWriter(System.out, true);
 
-    if (args.length != 4) {
+    if (args.length != ARGS_SIZE) {
       System.err.printf("Error: Expected 4 parameters, received %d", args.length);
       return;
-    }
+    } // length check
 
     int enc = 2;
     int cae = 2;
@@ -20,84 +32,81 @@ public class Cipher {
     String ca = new String("-caesar");
     String ve = new String("-vigenere");
 
-    String[] a = new String[3];
-    for (int i = 0; i < 4; i++) {
+    String[] a = new String[ARGS_SIZE - 1];
+    for (int i = 0; i < ARGS_SIZE; i++) {
       if (en.equals(args[i])) {
         enc = 1;
         a = CipherUtils.removeElement(args, i);
         break;
-      }
-      else if (dc.equals(args[i])) {
+      } else if (dc.equals(args[i])) {
         enc = 0;
         a = CipherUtils.removeElement(args, i);
         break;
-      }
-    }
-    
+      } // decrypt call
+    } // iterate
+
     if (enc == 2) {
-     System.err.println("Error: No valid action specified.  Legal values are '-encode' and '-decode'");
-     return;
-    }
+      System.err.printf("Error: No valid action specified.  "
+          + "Legal values are '-encode' and '-decode'");
+      return;
+    } // no encode or decode call
 
     String[] b = new String[2];
-    for (int i = 0; i < 3; i++) {
+    for (int i = 0; i <= 2; i++) {
       if (ca.equals(a[i])) {
         cae = 1;
         b = CipherUtils.removeElement(a, i);
         break;
-      }
-      else if (ve.equals(a[i])) {
+      } else if (ve.equals(a[i])) {
         cae = 0;
         b = CipherUtils.removeElement(a, i);
         break;
-      }
-    }
+      } // check for action call (vig)
+    } // iterate
 
     if (cae == 2) {
-      System.err.println("Error: No valid action specified.  Legal values are '-caesar' and '-vigenere'");
+      System.err.printf("Error: No valid action specified.  "
+          + "Legal values are '-caesar' and '-vigenere'");
       return;
-    }
+    } // check if action call
 
     for (int num = 0; num < 2; num++) {
       String s = b[num];
-      int str_len = s.length();
-      for (int i = 0; i < str_len; i++)
-    {
-      int c = CipherUtils.letter2int(s.charAt(i));
-      if (c < 0 || c > 25)
-      {
-        System.err.println("Error: String contains characters other than lowercase letters.");
-        return;
-      }
-    }
-    }
+      int strLen = s.length();
+      for (int i = 0; i < strLen; i++) {
+        int c = CipherUtils.letter2int(s.charAt(i));
+        if (c < 0 || c >= CipherUtils.NUM_LETTERS) {
+          System.err.printf("Error: String contains characters other than lowercase letters.");
+          return;
+        } // illegal char check
+      } // iterate
+    } // iterate
 
     if (cae == 1) {
       if (b[1].length() != 1) {
-        System.err.println("Error: Caesar ciphers require a one-character key");
+        System.err.printf("Error: Caesar ciphers require a one-character key");
         return;
-      }
-      else if (enc == 1) {
+      } else if (enc == 1) {
         String result = CipherUtils.caesarEncrypt(b[0], b[1].charAt(0));
-        pen.println(result);
-      }
-      else if (enc == 0) {
+        pen.printf(result);
+      } else if (enc == 0) {
         String result = CipherUtils.caesarDecrypt(b[0], b[1].charAt(0));
-        pen.println(result);
-      }
-    }
-    else if (cae == 0) {
+        pen.printf(result);
+      } // if decrypt
+    } else if (cae == 0) {
+      if (b[1].length() < 1) {
+        System.err.printf("Error: Invalid key");
+        return;
+      } // if key length smaller than 1
       if (enc == 1) {
         String result = CipherUtils.vigenereEncrypt(b[0], b[1]);
-        pen.println(result);
-      }
-      else if (enc == 0) {
+        pen.printf(result);
+      } else if (enc == 0) {
         String result = CipherUtils.vigenereDecrypt(b[0], b[1]);
-        pen.println(result);
-      }
-    }
-
+        pen.printf(result);
+      } // if decrypt
+    } // if vigenere
 
     pen.close();
-  }
-}
+  } // main
+} // Cipher()
