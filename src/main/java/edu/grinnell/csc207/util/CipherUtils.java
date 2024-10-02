c97a65dc582601d82fe7bcb5210a9d6260eabcfd
gold-leo <goldmanleo@grinnell.edu>
2024-10-02 16:41:11 -0500

    fixed utils

--- src/main/java/edu/grinnell/csc207/util/CipherUtils.java
+++ src/main/java/edu/grinnell/csc207/util/CipherUtils.java
@@ -1,107 +1,158 @@
 package edu.grinnell.csc207.util;
 
+
+/**
+ * Utils for all ciphers.
+ */
 public class CipherUtils {
+
+  /**
+   * The number of lowercase letters.
+   */
+  public static final int NUM_LETTERS = 26;
+
+  /**
+   * Remove an element.
+   * @param strs
+   * @param index
+   * @return array with removed element
+   */
   public static String[] removeElement(String[] strs, int index) {
     int l = strs.length;
-    String[] n_strs = new String[l - 1];
+    String[] nStrs = new String[l - 1];
     for (int i = 0, n = 0; i < l; i++) {
       if (i == index) {
         continue;
-      }
-      else {
-        n_strs[n++] = strs[i];
-      }
-    }
-    return n_strs;
-  }
+      } else {
+        nStrs[n++] = strs[i];
+      } // else
+    } // iterate
+    return nStrs;
+  } // removeElement()
 
+  /**
+   * Convert a char to int.
+   * @param letter
+   * @return int
+   */
   public static int letter2int(char letter) {
     return (int) letter - (int) 'a';
-  }
+  } // letter2int()
 
+  /**
+   * Convert an int to char.
+   * @param i
+   * @return char
+   */
   public static char int2letter(int i) {
     return (char) (i + (int) 'a');
-  }
+  } // int2letter()
 
+  /**
+   * Encrypt a character.
+   * @param ch
+   * @param key
+   * @return encrypted char
+   */
   private static char charEncrypt(char ch, char key) {
     int n = letter2int(ch);
     n += letter2int(key);
-    if (n > 25)
-    {
-      n -= 26;
-    }
+    if (n >= NUM_LETTERS) {
+      n -= NUM_LETTERS;
+    } // circle
     return int2letter(n);
-  }
+  } // charEncrypt()
 
+  /**
+   * Decrypt a character.
+   * @param ch
+   * @param key
+   * @return decrypted char
+   */
   private static char charDecrypt(char ch, char key) {
     int n = letter2int(ch);
     n -= letter2int(key);
-    if (n < 0)
-    {
-      n += 26;
-    }
+    if (n < 0) {
+      n += NUM_LETTERS;
+    } // circle
     return int2letter(n);
-  }
+  } // charDecrypt()
 
+  /**
+   * Encrypt a caesar string.
+   * @param str
+   * @param letter
+   * @return encrypted string
+   */
   public static String caesarEncrypt(String str, char letter) {
     int l = str.length();
-    String str_en = new String();
-    for (int i = 0; i < l; i++)
-    {
+    String strEn = new String();
+    for (int i = 0; i < l; i++) {
       char ch = charEncrypt(str.charAt(i), letter);
-      str_en += ch;
-    }
-    return str_en;
-  }
+      strEn += ch;
+    } // iterate
+    return strEn;
+  } // caesarEncrypt()
 
+  /**
+   * Decrypt a caesar cipher.
+   * @param str
+   * @param letter
+   * @return decrypted cipher
+   */
   public static String caesarDecrypt(String str, char letter) {
     int l = str.length();
-    String str_dc = new String();
-    for (int i = 0; i < l; i++)
-    {
+    String strDc = new String();
+    for (int i = 0; i < l; i++) {
       char ch = charDecrypt(str.charAt(i), letter);
-      str_dc += ch;
-    }
-    return str_dc;
-  }
+      strDc += ch;
+    } // iterate
+    return strDc;
+  } // caesarDecrypt()
 
+  /**
+   * Encrypt a vigenere string.
+   * @param str
+   * @param key
+   * @return encrypted cipher
+   */
   public static String vigenereEncrypt(String str, String key) {
-    int k_len = key.length();
-    int s_len = str.length();
-    String str_en = new String();
-    for (int num = 0; num < s_len; num += k_len)
-    {
-      int n = s_len - num;
-      if (n > k_len)
-      {
-        n = k_len;
-      }
-      for (int i = 0; i < n; i++)
-      {
+    int kLen = key.length();
+    int sLen = str.length();
+    String strEn = new String();
+    for (int num = 0; num < sLen; num += kLen) {
+      int n = sLen - num;
+      if (n > kLen) {
+        n = kLen;
+      } // check
+      for (int i = 0; i < n; i++) {
         char ch = charEncrypt(str.charAt(num + i), key.charAt(i));
-        str_en += ch;
-      }
-    }
-    return str_en;
-  }
+        strEn += ch;
+      } // check
+    } // iterate through string
+    return strEn;
+  } // vigenereEncrypt()
 
+  /**
+   * Decrypt a vigenere cipher.
+   * @param str
+   * @param key
+   * @return decrypted cipher
+   */
   public static String vigenereDecrypt(String str, String key) {
-    int k_len = key.length();
-    int s_len = str.length();
-    String str_en = new String();
-    for (int num = 0; num < s_len; num += k_len)
-    {
-      int n = s_len - num;
-      if (n > k_len)
-      {
-        n = k_len;
-      }
-      for (int i = 0; i < n; i++)
-      {
+    int kLen = key.length();
+    int sLen = str.length();
+    String strEn = new String();
+    for (int num = 0; num < sLen; num += kLen) {
+      int n = sLen - num;
+      if (n > kLen) {
+        n = kLen;
+      } // check
+      for (int i = 0; i < n; i++) {
         char ch = charDecrypt(str.charAt(num + i), key.charAt(i));
-        str_en += ch;
-      }
-    }
-    return str_en;
-  }
-}
+        strEn += ch;
+      } // check
+    } // iterate through string
+    return strEn;
+  } // vigenereDecrypt()
+} // CipherUtils
